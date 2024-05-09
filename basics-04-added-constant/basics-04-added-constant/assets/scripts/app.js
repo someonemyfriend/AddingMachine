4055d52ec903ea2d2aff42cea0d39254c94be539
Vasilia Staykova <someonemyfriend@gmail.com>
2024-05-09 16:21:56 +0300

    first resources

---
+++ basics-04-added-constant/basics-04-added-constant/assets/scripts/app.js
@@ -0,0 +1,10 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+
+outputResult(currentResult, calculationDescription);
+
+//console.log('unit24');
